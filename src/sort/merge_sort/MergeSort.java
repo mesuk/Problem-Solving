eb11e44a61965a96f06ca6b5185f4456978d75ba
mesuk <mesukcse08@gmail.com>
2019-02-10 00:40:46 +0600

    Merge sort and sudo code added

--- src/sort/merge_sort/MergeSort.java
+++ src/sort/merge_sort/MergeSort.java
@@ -9,55 +9,57 @@ public class MergeSort implements ISort {
 
     @Override
     public Integer[] sort(Integer[] dataSet) {
-         mergeSort(dataSet,0,dataSet.length-1);
+        mergeSort(dataSet, 0, dataSet.length - 1);
         return dataSet;
     }
 
-    void mergeSort(Integer Arr[], int start, int end) {
+    void mergeSort(Integer totalArray[], int start, int end) {
 
-        if(start < end) {
+        if (start < end) {
             int mid = (start + end) / 2;
-            mergeSort(Arr, start, mid);
-            mergeSort(Arr, mid+1, end);
-            merge(Arr, start, mid, end);
+            mergeSort(totalArray, start, mid);
+            mergeSort(totalArray, mid + 1, end);
+            mergeTwoSortedArray(totalArray, start, mid, end);
         }
     }
 
-    void merge(Integer Arr[], int start, int mid, int end) {
+    void mergeTwoSortedArray(Integer totalArray[], int start, int mid, int end) {
 
         // create a temp array
         int temp[] = new int[end - start + 1];
 
         // crawlers for both intervals and for temp
-        int i = start, j = mid+1, k = 0;
+        int i = start, j = mid + 1, k = 0;
 
         // traverse both arrays and in each iteration add smaller of both elements in temp
-        while(i <= mid && j <= end) {
-            if(Arr[i] <= Arr[j]) {
-                temp[k] = Arr[i];
-                k += 1; i += 1;
+        while (i <= mid && j <= end) {
+            if (totalArray[i] <= totalArray[j]) {
+                temp[k] = totalArray[i];
+                i += 1;
+            } else {
+                temp[k] = totalArray[j];
+                j += 1;
             }
-            else {
-                temp[k] = Arr[j];
-                k += 1; j += 1;
-            }
+            k += 1;
         }
 
         // add elements left in the first interval
-        while(i <= mid) {
-            temp[k] = Arr[i];
-            k += 1; i += 1;
+        while (i <= mid) {
+            temp[k] = totalArray[i];
+            k += 1;
+            i += 1;
         }
 
         // add elements left in the second interval
-        while(j <= end) {
-            temp[k] = Arr[j];
-            k += 1; j += 1;
+        while (j <= end) {
+            temp[k] = totalArray[j];
+            k += 1;
+            j += 1;
         }
 
         // copy temp to original interval
-        for(i = start; i <= end; i += 1) {
-            Arr[i] = temp[i - start];
+        for (i = start; i <= end; i += 1) {
+            totalArray[i] = temp[i - start];
         }
     }
 }
