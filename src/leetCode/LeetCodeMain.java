2f5cb2730d980ed9225061734db83a31a36e3a84
mesuk <mesukcse08@gmail.com>
2019-03-04 01:01:22 +0600

    LeetCode 509,414 done

--- src/leetCode/LeetCodeMain.java
+++ src/leetCode/LeetCodeMain.java
@@ -22,5 +22,46 @@ public class LeetCodeMain {
         System.out.println("New base :"+newBase);
         System.out.println("Converted number :"+convertedNumber);
 
+        System.out.println();
+        number=25;
+        boolean isPerfectNumber=leetCodeProblems.checkPerfectNumber(number);
+        System.out.println("Number :"+number +" isPerfect : "+isPerfectNumber);
+
+
+        System.out.println();
+        number=28;
+        isPerfectNumber=leetCodeProblems.checkPerfectNumber(number);
+        System.out.println("Number :"+number +" isPerfect : "+isPerfectNumber);
+
+        System.out.println();
+//        int[] nums={3,9,5,1,2};
+//        int[] nums={2, 2, 3, 1};
+//        int[] nums={1,2,2};
+//        int[] nums={5,2,2};
+//        int[] nums={1,2,-2147483648};
+        int[] nums={1,-2147483648,2};
+        int result=leetCodeProblems.thirdMax(nums);
+        System.out.println("Number :"+result);
+
+        System.out.println();
+        int thNumber=3;
+        int fibNumber=leetCodeProblems.fib(thNumber);
+        System.out.println(thNumber+" Fibonacci Number  :"+fibNumber);
+
+        System.out.println();
+        thNumber=6;
+        fibNumber=leetCodeProblems.fib(thNumber);
+        System.out.println(thNumber+" Fibonacci Number  :"+fibNumber);
+
+
+        System.out.println();
+        thNumber=10;
+        fibNumber=leetCodeProblems.fib(thNumber);
+        System.out.println(thNumber+" Fibonacci Number  :"+fibNumber);
+
+        System.out.println();
+        thNumber=12;
+        fibNumber=leetCodeProblems.fib(thNumber);
+        System.out.println(thNumber+" Fibonacci Number  :"+fibNumber);
     }
 }
--- src/leetCode/LeetCodeProblems.java
+++ src/leetCode/LeetCodeProblems.java
@@ -3,34 +3,121 @@ package leetCode;
 public class LeetCodeProblems {
 
     //504. Base 7
-    public String baseConversion(int num){
+    public String baseConversion(int num) {
 
-        int base=7;
-        if(num==0){
-            return ""+0;
+        int base = 7;
+        if (num == 0) {
+            return "" + 0;
         }
 
-        boolean isNegativeNumber=(num<0);
+        boolean isNegativeNumber = (num < 0);
 
-        if(isNegativeNumber){
-            num=num*(-1);
+        if (isNegativeNumber) {
+            num = num * (-1);
         }
 
-        StringBuilder stringBuilder=new StringBuilder("");
+        StringBuilder stringBuilder = new StringBuilder("");
 
-        while (num>=base){
-            int reminder=num%base;
+        while (num >= base) {
+            int reminder = num % base;
             stringBuilder.append(reminder);
-            num=num/base;
+            num = num / base;
         }
         stringBuilder.append(num);
 
-        if(isNegativeNumber){
+        if (isNegativeNumber) {
             stringBuilder.append("-");
         }
 
-        String convertedNumber=stringBuilder.reverse().toString();
+        String convertedNumber = stringBuilder.reverse().toString();
 
         return convertedNumber;
+    }
+
+    //507. Perfect Number
+
+    public boolean checkPerfectNumber(int num) {
+
+        if (num <= 2) {
+            return false;
+        }
+
+        int sumNumber = 0;
+        for (int index = 1; index <= (num / 2); index++) {
+            if (num % index == 0) {
+                sumNumber += index;
+            }
+        }
+
+        return num == sumNumber;
+    }
+
+    //414. Third Maximum Number
+    public int thirdMax(int[] nums) {
+
+        Integer firstMax = null, secondMax = null, thirdMax = null;
+
+        for (int index = 0; index < nums.length; index++) {
+            if (firstMax == null || nums[index] >= firstMax) {
+
+                if (firstMax != null && nums[index] == firstMax) {
+                    continue;
+                }
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = nums[index];
+
+
+            } else if (secondMax == null || nums[index] >= secondMax) {
+                if (secondMax != null && nums[index] == secondMax) {
+                    continue;
+                }
+
+                thirdMax = secondMax;
+                secondMax = nums[index];
+            } else if (thirdMax == null || nums[index] >= thirdMax) {
+                thirdMax = nums[index];
+            }
+        }
+
+        if (thirdMax != null) {
+            return thirdMax;
+        }
+
+        return firstMax;
+    }
+
+    //509. Fibonacci Number
+    public int fib(int N) {
+        //return calCulateFibonakki(N);
+
+        System.out.println();
+        if (N <= 1)
+            return N;
+
+        int a = 0, b = 1;
+
+        while (N-- > 1) {
+            System.out.println("N :"+N);
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+        System.out.println();
+
+        return b;
+
+    }
+
+
+    // This can occure stackoverflow error
+    public int calCulateFibonakki(int N) {
+        if (N == 1) {
+            return 0;
+        } else if (N == 2) {
+            return 1;
+        }
+
+        return calCulateFibonakki(N - 1) + calCulateFibonakki(N - 2);
     }
 }
