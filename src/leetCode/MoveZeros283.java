58878e42385175aa81a0c721bb0f930b6d886c71
mdsajedul.karim <mdsajedul.karim@bracits.com>
2022-01-11 10:11:41 +0600

    LeetCode 283 Move Zeros - Solved

--- src/leetCode/MoveZeros283.java
+++ src/leetCode/MoveZeros283.java
@@ -4,23 +4,33 @@ import java.util.Arrays;
 
 public class MoveZeros283 {
     public static void main(String[] args) {
+//        https://leetcode.com/problems/move-zeroes/submissions/
 
-        int [] nums = new int []{0,1,0,3,12};
+//        Input
+//                [73348,66106,-85359,53996,18849,-6590,-53381,-86613,-41065,83457,0]
+//        Output
+//                [73348,66106,53996,18849,83457,0,0,0,0,0,0]
+//        Expected
+//                [73348,66106,-85359,53996,18849,-6590,-53381,-86613,-41065,83457,0]
+
+//        int[] nums = new int[]{0, 1, 0, 3, 12};
+        int[] nums = new int[]{73348, 66106, -85359, 53996, 18849, -6590, -53381, -86613, -41065, 83457, 0};
         moveZeroes(nums);
         System.out.println(Arrays.toString(nums));
-
     }
 
     public static void moveZeroes(int[] nums) {
-        int [] tempNums = new int[nums.length];
-
-        int counter = 0;
+        int nonZeroPosition = 0;
         for (int i = 0; i < nums.length; i++) {
-            if(nums[i]>0){
-                tempNums[counter] = nums[i];
-                counter++;
+            if (nums[i] != 0) {
+                nums[nonZeroPosition] = nums[i];
+                nonZeroPosition++;
             }
         }
-        nums = tempNums;
+
+        for (int i = nonZeroPosition; i < nums.length; i++) {
+            nums[nonZeroPosition] = 0;
+            nonZeroPosition++;
+        }
     }
 }
