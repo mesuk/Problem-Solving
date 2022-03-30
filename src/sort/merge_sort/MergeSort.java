170f6a4a59bd0e9a0f5ec09f723854d16f6ce29a
mdsajedul.karim <mdsajedul.karim@bracits.com>
2022-03-30 14:29:24 +0600

    Unnecessary file removed

--- src/sort/merge_sort/MergeSort.java
+++ src/sort/merge_sort/MergeSort.java
@@ -3,36 +3,35 @@ package sort.merge_sort;
 import sort.ISort;
 
 public class MergeSort implements ISort {
-    public MergeSort(boolean showData) {
 
-    }
-
+    //https://www.geeksforgeeks.org/merge-sort/
     @Override
     public Integer[] sort(Integer[] dataSet) {
         mergeSort(dataSet, 0, dataSet.length - 1);
         return dataSet;
     }
 
-    void mergeSort(Integer totalArray[], int start, int end) {
+    void mergeSort(Integer[] totalArray, int l, int r) {
 
-        if (start < end) {
-            int mid = (start + end) / 2;
-            mergeSort(totalArray, start, mid);
-            mergeSort(totalArray, mid + 1, end);
-            mergeTwoSortedArray(totalArray, start, mid, end);
+        if (l < r) {
+            int mid = l + (r - l) / 2;
+            mergeSort(totalArray, l, mid);
+            mergeSort(totalArray, mid + 1, r);
+            merge(totalArray, l, mid, r);
         }
     }
 
-    void mergeTwoSortedArray(Integer totalArray[], int start, int mid, int end) {
+    void merge(Integer[] totalArray, int l, int m, int r) {
 
-        // create a temp array
-        int temp[] = new int[end - start + 1];
+        // Problem: Need an extra space, length n
+        // create a new temp array
+        int[] temp = new int[r - l + 1];
 
         // crawlers for both intervals and for temp
-        int i = start, j = mid + 1, k = 0;
+        int i = l, j = m + 1, k = 0;
 
         // traverse both arrays and in each iteration add smaller of both elements in temp
-        while (i <= mid && j <= end) {
+        while (i <= m && j <= r) {
             if (totalArray[i] <= totalArray[j]) {
                 temp[k] = totalArray[i];
                 i += 1;
@@ -44,23 +43,21 @@ public class MergeSort implements ISort {
         }
 
         // add elements left in the first interval
-        while (i <= mid) {
+        while (i <= m) {
             temp[k] = totalArray[i];
             k += 1;
             i += 1;
         }
 
         // add elements left in the second interval
-        while (j <= end) {
+        while (j <= r) {
             temp[k] = totalArray[j];
             k += 1;
             j += 1;
         }
-
-
         // copy temp to original interval
-        for (i = start; i <= end; i += 1) {
-            totalArray[i] = temp[i - start];
+        for (i = l; i <= r; i += 1) {
+            totalArray[i] = temp[i - l];
         }
     }
 }
